0b58ba88ce5b7ddc96fdc90d56b0bd1ffe56a6e8
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-12-24 15:21:13 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import MyApp from "./main";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
+        <MyApp />
       </header>
     </div>
   );
