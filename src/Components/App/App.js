89fd7df75f320d1fb894a753aa3d91bc923a6cbe
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-12-29 15:33:33 +0200

    end lesson

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,50 @@
 import { Component } from "react";
-import Counter from "Components/Counter";
-import Dropdown from "Components/Dropdown";
-import Section from "Components/Section";
 import "./App.css";
-import ColorPicker from "Components/ColorPicker";
+// import Counter from "Components/Counter";
+// import Dropdown from "Components/Dropdown";
+// import Section from "Components/Section";
+// import ColorPicker from "Components/ColorPicker";
+import TodoList from "Components/TodoList";
+import initialTodos from "../../todos.json";
 
 // import colors from "../ColorPicker/colors.json";
-const colors = [
-  { label: "red", color: "#F44336" },
-  { label: "green", color: "#4CAF50" },
-  { label: "blue", color: "#2196F3" },
-  { label: "grey", color: "#607088" },
-  { label: "pink", color: "#E91E63" },
-  { label: "indigo", color: "#3F5085" },
-];
+// const colors = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "grey", color: "#607088" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F5085" },
+// ];
 
 class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = (todoId) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
+  };
+
   render() {
+    const { todos } = this.state;
+
+    const completedTodos = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
+
     return (
       <div className="app">
         {/* <Counter /> */}
         {/* <Dropdown /> */}
-        <ColorPicker colors={colors} />
+        {/* <ColorPicker colors={colors} /> */}
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo}>
+          <p>Total todos: {todos.length}</p>
+          <p>Completed todos: {completedTodos}</p>
+        </TodoList>
       </div>
     );
   }
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,14 +1,24 @@
 import "./TodoList.css";
-import { Component } from "react";
 
-class TodoList extends Component {
-  state = {
-    todos: [],
-  };
-
-  render() {
-    return <ul></ul>;
-  }
-}
+const TodoList = ({ todos, onDeleteTodo, children }) => {
+  return (
+    <>
+      <ul className="todo">
+        {todos.map(({ id, text }) => (
+          <li key={id} className="todo-item">
+            <p>{text}</p>
+            <button
+              type="button"
+              className="delete-btn"
+              onClick={() => onDeleteTodo(id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      {children}
+    </>
+  );
+};
 
 export default TodoList;
