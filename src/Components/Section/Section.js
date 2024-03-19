785c7978bb7dcac0ec8cf7ce51755782b08c2a17
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-03-19 11:48:29 +0200

    newLife

--- src/Components/Section/Section.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Section.css";
-
-const Section = ({ children }) => {
-  return <section className="container">{children}</section>;
-};
-
-export default Section;
